8292390fdd5e516f9e40d98a292c29e6cf83cb93
Chris <tomlinc98@gmail.com>
2024-09-24 13:24:30 +0100

    chore: Enable flask debug mode

--- app/config.py
+++ app/config.py
@@ -5,4 +5,3 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'b74cbe400be677ef6b4f21602d4899988a0eec844b06eab306cbba0cf2e07ea5'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///scrumjet.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
